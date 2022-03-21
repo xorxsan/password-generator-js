3f9cc24040830eb747cf8af978e783b9bc6fcc8b
Jorge Sanchez Aparicio <Jorge.SanchezAparicio@vistaprint.com>
2022-03-21 15:19:58 +0100

    Improve UI

--- webpack/src/App.tsx
+++ webpack/src/App.tsx
@@ -1,8 +1,16 @@
 import React, { useState } from "react";
-import { Container, Stack, Button, Grid, TextField } from "@mui/material";
+import {
+  Container,
+  Stack,
+  Button,
+  Grid,
+  TextField,
+  IconButton,
+} from "@mui/material";
 import { Box } from "@mui/system";
-
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import { generate_v0, generate_v1, generate_v2 } from "pkg";
+import { ContentCopy } from "@mui/icons-material";
 
 const App = (): JSX.Element => {
   const [masterPassword, setMasterPassword] = useState<string | undefined>(
@@ -45,6 +53,17 @@ const App = (): JSX.Element => {
     }
   };
 
+  const onClearAllClicked = (_event: React.MouseEvent<HTMLButtonElement>) => {
+    setMasterPassword("");
+    setSeed("");
+    setLengthV0(20);
+    setLengthV1(20);
+    setLengthV2(25);
+    setPasswordV0("");
+    setPasswordV1("");
+    setPasswordV2("");
+  };
+
   return (
     <Container>
       <Box marginTop={5}>
@@ -73,6 +92,9 @@ const App = (): JSX.Element => {
           <Button variant="outlined" onClick={onGeneratePasswordClicked}>
             Generate password
           </Button>
+          <Button variant="outlined" onClick={onClearAllClicked}>
+            Clear all
+          </Button>
         </Stack>
         <Stack spacing={2} marginTop={2}>
           <Grid container>
@@ -86,8 +108,15 @@ const App = (): JSX.Element => {
                 onChange={(e) => onChangeLength(e, setLengthV0)}
               />
             </Grid>
-            <Grid item xs={8}>
+            <Grid item xs={7}>
               <TextField hiddenLabel disabled fullWidth value={passwordV0} />
+            </Grid>
+            <Grid item xs={1}>
+              <CopyToClipboard text={passwordV0}>
+                <IconButton aria-label="Copy password v0 to clipboard">
+                  <ContentCopy />
+                </IconButton>
+              </CopyToClipboard>
             </Grid>
           </Grid>
           <Grid container>
@@ -101,8 +130,15 @@ const App = (): JSX.Element => {
                 onChange={(e) => onChangeLength(e, setLengthV1)}
               />
             </Grid>
-            <Grid item xs={8}>
+            <Grid item xs={7}>
               <TextField hiddenLabel disabled fullWidth value={passwordV1} />
+            </Grid>
+            <Grid item xs={1}>
+              <CopyToClipboard text={passwordV1}>
+                <IconButton aria-label="Copy password v1 to clipboard">
+                  <ContentCopy />
+                </IconButton>
+              </CopyToClipboard>
             </Grid>
           </Grid>
           <Grid container>
@@ -116,8 +152,15 @@ const App = (): JSX.Element => {
                 onChange={(e) => onChangeLength(e, setLengthV2)}
               />
             </Grid>
-            <Grid item xs={8}>
+            <Grid item xs={7}>
               <TextField hiddenLabel disabled fullWidth value={passwordV2} />
+            </Grid>
+            <Grid item xs={1}>
+              <CopyToClipboard text={passwordV2}>
+                <IconButton aria-label="Copy password v2 to clipboard">
+                  <ContentCopy />
+                </IconButton>
+              </CopyToClipboard>
             </Grid>
           </Grid>
         </Stack>
